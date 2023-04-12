5d376db84c52f338b823503a9ecbe6e9be467237
Bodgar Miranda <Bodgar.Miranda@walmart.com>
2023-04-12 09:50:28 -0600

    Initial Commit

---
+++ MovieDatabase/MovieDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseApp.swift
+//  MovieDatabase
+//
+//  Created by Bodgar Jair Espinosa Miranda on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
