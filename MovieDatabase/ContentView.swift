83049b8b4622ddd3ff2c51c83e36054fb664dd50
Bodgar Miranda <Bodgar.Miranda@walmart.com>
2023-04-12 10:58:14 -0600

    Start creating the home screen of this new app that contain the trending movies.

--- MovieDatabase/ContentView.swift
+++ MovieDatabase/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = MovieDBViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, test")
+            
         }
         .padding()
+        .onAppear {
+            viewModel.loadTrending()
+        }
     }
+}
+
+class MovieDBViewModel: ObservableObject {
+    @Published var homeScreen: [TrendingItem] = []
+    
+    init(){}
+    
+    func loadTrending() {
+        Task {
+            
+        }
+    }
+}
+
+struct TrendingItem: Identifiable, Decodable {
+    let adult: Bool
+    let id: Int
+    let poster_path: String
+    let title: String
+    let vote_average: Float
 }
 
 struct ContentView_Previews: PreviewProvider {
